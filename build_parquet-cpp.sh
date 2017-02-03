40ba7fbbd286a50c8a6228b2e16782fb1fd8299d
Marc Vertes <mvertes@free.fr>
2017-02-03 17:45:12 +0100

    debug for travis

--- build_parquet-cpp.sh
+++ build_parquet-cpp.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build_deps/parquet-cpp
 cd build_deps/parquet-cpp
-cmake -DPARQUET_BUILD_TESTS=Off -DCMAKE_BUILD_TYPE=Release ../../deps/parquet-cpp
+cmake --debug-output -DPARQUET_BUILD_TESTS=Off -DCMAKE_BUILD_TYPE=Release ../../deps/parquet-cpp
 make
