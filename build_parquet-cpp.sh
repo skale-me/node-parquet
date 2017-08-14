4f8b5daa0341b27dcc4ea0fd3d878014e234207a
Marc Vertes <mvertes@free.fr>
2017-08-14 15:36:58 +0200

    fix build of static lib dependencies

--- build_parquet-cpp.sh
+++ build_parquet-cpp.sh
@@ -1,9 +1,20 @@
 #!/bin/sh -e
 
-mkdir -p build_deps/parquet-cpp
-cd build_deps/parquet-cpp
+BUILD_DIR=$PWD/build_deps/parquet-cpp
+ARROW_EP=$BUILD_DIR/arrow_ep-prefix/src/arrow_ep-build
+BROTLI_EP=$ARROW_EP/brotli_ep/src/brotli_ep-install/lib
+
+export SNAPPY_STATIC_LIB=$ARROW_EP/snappy_ep/src/snappy_ep-install/lib/libsnappy.a
+export BROTLI_STATIC_LIB_ENC=$BROTLI_EP/libbrotlienc.a
+export BROTLI_STATIC_LIB_DEC=$BROTLI_EP/libbrotlidec.a
+export BROTLI_STATIC_LIB_COMMON=$BROTLI_EP/libbrotlicommon.a
+export ZLIB_STATIC_LIB=$ARROW_EP/zlib_ep/src/zlib_ep-install/lib/libz.a
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 cmake -DPARQUET_BUILD_TESTS=Off \
   -DPARQUET_MINIMAL_DEPENDENCY=ON \
+  -DPARQUET_ARROW_LINKAGE="static" \
   -DCMAKE_BUILD_TYPE=Release \
   ../../deps/parquet-cpp
 make
